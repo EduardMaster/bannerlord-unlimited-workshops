4d419f93358f8d9ec63d71588bd0f2361f80d284
EduardMaster <eduardkiller@hotmail.com>
2025-07-10 10:15:35 -0300

    pos build

--- posbuild.bat
+++ posbuild.bat
@@ -1,3 +1,10 @@
 set "MOD_DEST=C:\Program Files (x86)\Steam\steamapps\common\Mount & Blade II Bannerlord\Modules\UnlimitedWorkshops"
+if not exist "%MOD_DEST%" (
+    mkdir "%MOD_DEST%"
+)
 xcopy resources "%MOD_DEST%" /s /e /y
-xcopy bin\Win64_Shipping_Client\net472\*.dll "%MOD_DEST%\bin\Win64_Shipping_Client" /s /e /y
+set "BIN_DEST=%MOD_DEST%\bin\Win64_Shipping_Client"
+if not exist "%BIN_DEST%" (
+    mkdir "%BIN_DEST%"
+)
+xcopy bin\Win64_Shipping_Client\net472\*.dll "%BIN_DEST%" /s /e /y
