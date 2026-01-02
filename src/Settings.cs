1b478c8727715dfc207e9e693edcdaca64c7f3e8
EduardMaster <eduardkiller@hotmail.com>
2026-01-02 15:20:27 -0300

    usando configuraçao glolbal

--- src/Settings.cs
+++ src/Settings.cs
@@ -1,11 +1,11 @@
-using MCM.Abstractions.Base.PerSave;
+
 using MCM.Abstractions.Attributes;
 using MCM.Abstractions.Attributes.v2;
-
-//using MCM.Abstractions.Base.Global;
+//using MCM.Abstractions.Base.PerSave;
+using MCM.Abstractions.Base.Global;
 namespace UnlimitedWorkshops
 {
-    public class Settings : AttributePerSaveSettings<Settings>
+    public class Settings : AttributeGlobalSettings<Settings> // AttributePerSaveSettings
     {
         public override string Id => "Unlimited_Workshops_v1";
         public override string DisplayName => "Unlimited Workshops";
@@ -13,7 +13,7 @@ namespace UnlimitedWorkshops
 
         //public override string FormatType => "json"; // Works only for global settings
 
-        [SettingPropertyInteger("Workshops per Clan Level", 1, 30, HintText = "Set how many workshops slots available per Clan level.", RequireRestart = false, Order = 0)]
+        [SettingPropertyInteger("Workshops per Clan Level", 2, 30, HintText = "Set how many workshops slots available per Clan level.", RequireRestart = false, Order = 0)]
         [SettingPropertyGroup("Main")]
         public int WorkshopsPerClanLevel { get; set; } = 5;
 
